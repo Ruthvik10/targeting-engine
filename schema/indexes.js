e057413f941568aa804d5c181b49e62795ae57fd
Ruthvik K S <rk99219@gmail.com>
2024-09-14 14:58:56 +0530

    Modified the indexes.

--- schema/indexes.js
+++ schema/indexes.js
@@ -1,12 +1,16 @@
+db.campaigns.createIndex({ status: 1 });
+
 db.campaigns.createIndex(
-  { status: 1, "targeting.includeCountry": 1 },
+  { "targeting.includeCountry": 1 },
   { collation: { locale: "en", strength: 2 } }
 );
+
 db.campaigns.createIndex(
-  { status: 1, "targeting.includeApp": 1 },
+  { "targeting.includeOS": 1 },
   { collation: { locale: "en", strength: 2 } }
 );
+
 db.campaigns.createIndex(
-  { status: 1, "targeting.includeOS": 1 },
+  { "targeting.includeApp": 1 },
   { collation: { locale: "en", strength: 2 } }
 );
